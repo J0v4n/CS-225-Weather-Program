023ea53f772a5d0e367936c63fad3a1efceb6f02
jlamberson1 <71022873+jlamberson1@users.noreply.github.com>
2022-05-04 12:24:33 -0400

    testing in progress for screen switching using fxml files
    currently, fxml loader has not been finished as it does not find the file even though it exists

--- src/SceneController.java
+++ src/SceneController.java
@@ -7,16 +7,28 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.layout.*;
 
 
 /**
  * Responsible for controlling the main UI elements of the Simulation
  */
 public class SceneController implements Initializable{
+
+
     
 
     @FXML
     Button pressButton;
+
+    @FXML
+    Button screen1Button;
+
+    @FXML
+    Button screen2Button;
+
+    @FXML
+    FlowPane mainScreen;
 
   
     @Override
@@ -28,6 +40,16 @@ public class SceneController implements Initializable{
  
     public void helloWorld() {
         System.out.println("Hello World");
+    }
+
+    public void setScreen1(){
+        FxmlLoader sceneLoader = new FxmlLoader();
+        Pane view = sceneLoader.getPage("Screen1");
+        mainScreen = (FlowPane) view;
+    }
+
+    public void setScreen2(){
+
     }
     
 }
