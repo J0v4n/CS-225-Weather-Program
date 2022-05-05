2c9199b6d590155125e12c8b499cf4a55e8ef734
davids251 <99621985+davids251@users.noreply.github.com>
2022-05-04 21:14:14 -0400

    Update DailyReport.java

--- DailyReport.java
+++ DailyReport.java
@@ -4,38 +4,51 @@ import java.util.Objects;
  * @author David Smirnov
  */
 public class DailyReport {
+    private String data;
+    private String stationName;
+    private String stationId;
     private double AverageWindSpeed;
     private double MaxWindSpeed;
     private double DailyPrecipitation;
-    public DailyReport(double aWS, double mWS, double dPersipitation){
+    public DailyReport(String info,String sN,String sID,double aWS, double mWS, double dPersipitation){
+        this.data=info;
+        this.stationName=sN;
+        this.stationId=sID;
         this.AverageWindSpeed=aWS;
         this.MaxWindSpeed=mWS;
         this.DailyPrecipitation=dPersipitation;
     }
     public DailyReport(){
+        data="";
+        stationName="";
+        stationId="";
         AverageWindSpeed=0;
         MaxWindSpeed=0;
         DailyPrecipitation=0;
     }
-    private double getAverageWindSpeed(){
-        return AverageWindSpeed;
+    public String getData(){
+        return data;
     }
-    private double getMaxWindSpeed(){
-        return MaxWindSpeed;
+    public String getStationName(){
+        return stationName;
     }
-    private double getDailyPrecipitation(){
-        return DailyPrecipitation;
+    public String getStationId(){
+        return stationId;
     }
+    private double getAverageWindSpeed(){return AverageWindSpeed;}
+    private double getMaxWindSpeed(){return MaxWindSpeed;}
+    private double getDailyPrecipitation(){return DailyPrecipitation;}
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         DailyReport that = (DailyReport) o;
-        return Double.compare(that.AverageWindSpeed, AverageWindSpeed) == 0 && Double.compare(that.MaxWindSpeed, MaxWindSpeed) == 0 && Double.compare(that.DailyPrecipitation, DailyPrecipitation) == 0;
+        return Double.compare(that.AverageWindSpeed, AverageWindSpeed) == 0 && Double.compare(that.MaxWindSpeed, MaxWindSpeed) == 0 && Double.compare(that.DailyPrecipitation, DailyPrecipitation) == 0 && Objects.equals(data, that.data) && Objects.equals(stationName, that.stationName) && Objects.equals(stationId, that.stationId);
     }
+
     @Override
     public String toString(){
-        return ""+AverageWindSpeed+""+MaxWindSpeed+""+DailyPrecipitation;
+        return ""+data+""+""+stationId+""+stationName+""+AverageWindSpeed+""+MaxWindSpeed+""+DailyPrecipitation;
     }
 }
