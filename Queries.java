398f4a119171fbcd7218c8db3e36254733aea4fd
Carlos Rodriguez <72759819+carlosRodriguez0715@users.noreply.github.com>
2022-05-11 09:43:03 -0400

    getSpecificReport query fully functional

--- Queries.java
+++ Queries.java
@@ -1,43 +1,23 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Iterator;
+import java.time.LocalDate;
 import java.util.List;
 
 public class Queries {
    	
-	//Dummy Instance
 	Storage storage = new Storage();
 	private List<DailyReport> d;
 	private List<MonthlyReport> m;
-	//Dummy Constructor
+	
+	//Constructor
 	public Queries(){
 		this.d = this.storage.getDailyList();
 		this.m = this.storage.getMonthlyList();
 	}
-	
-	public List<DailyReport> getDailyReportsInAMonth(String date){
-		return null;
-	}
-	
-	public List getStationReports(String year, boolean isDaily){
-		return null;
-	}
-	
+		
 	/**
-	 * PROCEDURE:
-	 * -Sort dailylist by date
-	 * -Binary search middle index
-	 * -Return if middle index equals
-	 * -Else evaluate charsAt(i)
-	 * -if matching cities but
-	 * different dates compare by date 
-	 * -if <, ignore leftSide
-	 * -if >, ignore rightSide
-	 * 
-	 * 
 	 * @author: Carlos Rodriguez
-	 * Uses binary search to retrieve a specific report
-	 * on a specific date.
+	 * Uses binary search to find specific station and
+	 * linear search in matched station to retrieve a 
+	 * specific report on a specific date.
 	 * 
 	 * @param station: the desired station of the report.
 	 * @param date: the desired date of the report.
@@ -48,79 +28,89 @@ public class Queries {
 		DailyReport foundReport = null;
 		int startingIdx = 0;
 		int endingIdx = d.size() - 1;
-		boolean matchingStation = false;
 		
-		while(startingIdx <= endingIdx && matchingStation) {
+		while(startingIdx <= endingIdx) {
 			//Find the list's midpoint
 			int middleIdx = startingIdx + (endingIdx - startingIdx) / 2;
 			String currStation = d.get(middleIdx).getDailyStationName();
 			String currDate = d.get(middleIdx).getData();
-			
+			//System.out.println(d.get(middleIdx).toString());
 			//Matching specific element
-			if(currStation.equals(station) && currDate.equals(date)) {
+			if((station.equals(currStation)) && (date.equals(currDate))) {
 				foundReport = d.get(middleIdx);
-				matchingStation = true;
 				return foundReport;
 			}
-			//Further checking
 			else {
-				//-1 to the left, 1 to the right
-				int direction = -1;
-				//Station matching
+				//If station was found
 				if(currStation.equals(station)) {
-					//Check for date through linear search
-					String[] dateGiven = date.split("/");
-					String[] currD = currDate.split("/");
-					if(Integer.parseInt(currD[2]) < Integer.parseInt(dateGiven[2])) {
-						if(Integer.parseInt(currD[0]) < Integer.parseInt(dateGiven[0])) {
-							if(Integer.parseInt(currD[0]) < Integer.parseInt(dateGiven[0])) {
-								direction = -1;
-							}
-						}
+					String[] given = date.split("/");
+					String[] curr = currDate.split("/"); 
+					int[] givenI = new int[given.length];
+					int[] currI = new int[curr.length];
+					
+					for(int i=0; i<given.length; i++) {
+						givenI[i] = Integer.parseInt(given[i]);
+						currI[i] = Integer.parseInt(curr[i]);
 					}
-					else {
-						direction = 1;
-					}
+					
+					//given
+					LocalDate date1 = LocalDate.of(givenI[2], givenI[0], givenI[1]);
+					//found
+					LocalDate date2 = LocalDate.of(currI[2], currI[0], currI[1]);
+					boolean givenIsBefore = date1.isBefore(date2);
+					boolean elementFound = false;
+					int find = middleIdx;
 					//Linear search
-					if(direction == -1) {
-						while(middleIdx > startingIdx && !matchingStation) {
-							middleIdx--;
-							currStation = this.d.get(middleIdx).getDailyStationName();
-							currDate = this.d.get(middleIdx).getData();
-							if(currStation.equals(station) && currDate.equals(date)) {
-								foundReport = d.get(middleIdx);
-								matchingStation = true;
+					//backtrack
+					if(givenIsBefore) {
+						while(find > startingIdx && !elementFound) {
+							find--;
+							currDate = d.get(find).getData();
+							if(currDate.equals(date)) {
+								elementFound = true;
+								foundReport = this.d.get(find);
 								return foundReport;
 							}
 						}
 					}
+					//front
 					else {
-						while(middleIdx < endingIdx && !matchingStation) {
-							middleIdx++;
-							currStation = this.d.get(middleIdx).getDailyStationName();
-							currDate = this.d.get(middleIdx).getData();
-							if(currStation.equals(station) && currDate.equals(date)) {
-								foundReport = d.get(middleIdx);
-								matchingStation = true;
+						while(find < endingIdx && !elementFound) {
+							find++;
+							currDate = d.get(find).getData();
+							if(currDate.equals(date)) {
+								elementFound = true;
+								foundReport = this.d.get(find);
 								return foundReport;
 							}
 						}
 					}
 				}
-				//Not matching station
+				//Not Matching
 				else {
-					
+					int c = 0;
+					while(c < currStation.length() && c < station.length()
+							&& currStation.charAt(c) == station.charAt(c)) {
+						c++;
+					}
+					//Lower End
+					if(currStation.charAt(c) > station.charAt(c)) {
+						endingIdx = middleIdx - 1;
+					}
+					//Higher
+					else {
+						startingIdx = middleIdx + 1;
+					}
 				}
 			}
 		}
-		return foundReport;
+		return null;
 	}
 
 	public static void main(String[] args) {
 		Queries aQ = new Queries();
-		DailyReport aRep = aQ.getSpecificDailyReport("BEDFORD HANSCOM FIELD", "1/1/2021");
+		DailyReport aRep = aQ.getSpecificDailyReport("BOSTON", "1/31/2022");
 		System.out.println(aRep.toString());
-			
 	}
 	
 }
