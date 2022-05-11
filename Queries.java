91434b850f964b08b63421cb646e668accfa4e56
Carlos Rodriguez <72759819+carlosRodriguez0715@users.noreply.github.com>
2022-05-11 18:25:16 -0400

    specificDaily, specificMonthly, stationsNames done

--- Queries.java
+++ Queries.java
@@ -1,9 +1,11 @@
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Queries {
    	
-	Storage storage = new Storage();
+	private Storage storage = new Storage();
 	private List<DailyReport> d;
 	private List<MonthlyReport> m;
 	
@@ -26,91 +28,147 @@ public class Queries {
 	
 	public DailyReport getSpecificDailyReport(String station, String date) {
 		DailyReport foundReport = null;
-		int startingIdx = 0;
-		int endingIdx = d.size() - 1;
+		boolean matchingStation = false;
+		boolean reportWasFound = false;
+		int startIdx = 0;
+		int endIdx = this.d.size() - 1;
+		int middleIdx = 0;
+		String currStation = "";
+		String aDate = "";
 		
-		while(startingIdx <= endingIdx) {
-			//Find the list's midpoint
-			int middleIdx = startingIdx + (endingIdx - startingIdx) / 2;
-			String currStation = d.get(middleIdx).getDailyStationName();
-			String currDate = d.get(middleIdx).getData();
-			//System.out.println(d.get(middleIdx).toString());
-			//Matching specific element
-			if((station.equals(currStation)) && (date.equals(currDate))) {
-				foundReport = d.get(middleIdx);
-				return foundReport;
+		//Binary Search for matching station
+		while((startIdx <= endIdx) && (!reportWasFound)
+				&& (!matchingStation)) {
+			middleIdx = startIdx + (endIdx - startIdx) / 2;
+			currStation = this.d.get(middleIdx).getDailyStationName();
+			System.out.println(currStation);
+			
+			//Found matching station
+			if(currStation.equals(station)) {
+				matchingStation = true;
 			}
 			else {
-				//If station was found
-				if(currStation.equals(station)) {
-					String[] given = date.split("/");
-					String[] curr = currDate.split("/"); 
-					int[] givenI = new int[given.length];
-					int[] currI = new int[curr.length];
-					
-					for(int i=0; i<given.length; i++) {
-						givenI[i] = Integer.parseInt(given[i]);
-						currI[i] = Integer.parseInt(curr[i]);
-					}
-					
-					//given
-					LocalDate date1 = LocalDate.of(givenI[2], givenI[0], givenI[1]);
-					//found
-					LocalDate date2 = LocalDate.of(currI[2], currI[0], currI[1]);
-					boolean givenIsBefore = date1.isBefore(date2);
-					boolean elementFound = false;
-					int find = middleIdx;
-					//Linear search
-					//backtrack
-					if(givenIsBefore) {
-						while(find > startingIdx && !elementFound) {
-							find--;
-							currDate = d.get(find).getData();
-							if(currDate.equals(date)) {
-								elementFound = true;
-								foundReport = this.d.get(find);
-								return foundReport;
-							}
-						}
-					}
-					//front
-					else {
-						while(find < endingIdx && !elementFound) {
-							find++;
-							currDate = d.get(find).getData();
-							if(currDate.equals(date)) {
-								elementFound = true;
-								foundReport = this.d.get(find);
-								return foundReport;
-							}
-						}
-					}
+				//Binary search choice: to left or to right of list
+				int currIdx = 0;
+				while(((currIdx < currStation.length()) && (currIdx < station.length()))
+						&& (currStation.charAt(currIdx) == station.charAt(currIdx))) {
+					currIdx++;
 				}
-				//Not Matching
+				if(station.charAt(currIdx) > currStation.charAt(currIdx)) {
+					startIdx = middleIdx + 1; }
 				else {
-					int c = 0;
-					while(c < currStation.length() && c < station.length()
-							&& currStation.charAt(c) == station.charAt(c)) {
-						c++;
-					}
-					//Lower End
-					if(currStation.charAt(c) > station.charAt(c)) {
-						endingIdx = middleIdx - 1;
-					}
-					//Higher
-					else {
-						startingIdx = middleIdx + 1;
-					}
-				}
+					endIdx = middleIdx - 1; }
 			}
 		}
-		return null;
-	}
-
-	public static void main(String[] args) {
-		Queries aQ = new Queries();
-		DailyReport aRep = aQ.getSpecificDailyReport("BOSTON", "1/31/2022");
-		System.out.println(aRep.toString());
+		
+		//Once a matching station has been found
+		if(matchingStation) {
+			int c = middleIdx;
+			
+			String[] givenDate = date.split("/");
+			String[] currDate = this.d.get(c).getData().split("/");
+			LocalDate date1 = LocalDate.of(Integer.parseInt(givenDate[2]), Integer.parseInt(givenDate[0]), Integer.parseInt(givenDate[1]));
+			LocalDate date2 = LocalDate.of(Integer.parseInt(currDate[2]), Integer.parseInt(currDate[0]), Integer.parseInt(currDate[1]));
+		
+			boolean currLessThan = date2.isBefore(date1);
+			//Linear search forward
+			if(currLessThan) {
+				while(currLessThan && c <= endIdx) {
+					givenDate = date.split("/");
+					aDate = this.d.get(c).getData();
+					currDate = aDate.split("/");
+					currStation = this.d.get(c).getDailyStationName();
+					
+					date1 = LocalDate.of(Integer.parseInt(givenDate[2]), Integer.parseInt(givenDate[0]), Integer.parseInt(givenDate[1]));
+					date2 = LocalDate.of(Integer.parseInt(currDate[2]), Integer.parseInt(currDate[0]), Integer.parseInt(currDate[1]));
+					currLessThan = date2.isBefore(date1);
+					
+					if(currStation.equals(station) && aDate.equals(date)) {
+						reportWasFound = true;
+						foundReport = this.d.get(c);
+					}
+					c++;
+				}
+			}
+			//Linear search backward
+			else {
+				while(!currLessThan && c >= startIdx) {
+					givenDate = date.split("/");
+					aDate = this.d.get(c).getData();
+					currDate = aDate.split("/");
+					currStation = this.d.get(c).getDailyStationName();
+					
+					date1 = LocalDate.of(Integer.parseInt(givenDate[2]), Integer.parseInt(givenDate[0]), Integer.parseInt(givenDate[1]));
+					date2 = LocalDate.of(Integer.parseInt(currDate[2]), Integer.parseInt(currDate[0]), Integer.parseInt(currDate[1]));
+					currLessThan = date2.isBefore(date1);
+					
+					if(currStation.equals(station) && aDate.equals(date)) {
+						reportWasFound = true;
+						foundReport = this.d.get(c);
+					}
+					c--;
+				}
+			}
+		
+		}
+		
+		return foundReport;
 	}
 	
+	/**
+	 * */
+	
+	public List<MonthlyReport> getSpecificMonthlyReports(String month) {
+		List<MonthlyReport> foundReports = new ArrayList<MonthlyReport>();
+		for(int i=0; i<this.m.size(); i++) {
+			String[] currDate = this.m.get(i).getDate().split("/");
+			if(currDate[0].equals(month)) {
+				foundReports.add(this.m.get(i));
+			}
+		}
+		return Collections.unmodifiableList(foundReports);
+	}
+	
+	public List<List<String>> parseReports(String month, int isDaily){
+		List<List<String>> outerList = null;
+		if(isDaily == 1) {
+			List<MonthlyReport> aList = getSpecificMonthlyReports(month);
+			List<String> innerList = new ArrayList<String>();
+			outerList = new ArrayList<List<String>>();
+			for(int i=0; i<aList.size(); i++) {
+				innerList.add(aList.get(i).getStationName());
+				innerList.add(aList.get(i).getDate());
+				innerList.add(Double.toString(aList.get(i).getmonthlymonthlyMinTemp()));
+				innerList.add(Double.toString(aList.get(i).getmonthlymonthlyAvgTemp()));
+				innerList.add(Double.toString(aList.get(i).getmonthlymonthlyMaxTemp()));
+				innerList.add(Double.toString(aList.get(i).getmonthlymonthlyTotalPrecipitation()));
+			}
+			outerList.add(innerList);
+		}
+		return outerList;
+	}
+	
+	/**
+	 * @author: Carlos Rodriguez
+	 * 
+	 * Linearly searches for all available stations.
+	 * @return = List of String type with the names.
+	 */
+	public List<String> allStationsNames(){
+		List<String> allNames = new ArrayList<String>();
+		allNames.add(this.m.get(0).getStationName());
+ 		for(int i=1; i<this.m.size(); i++) {
+ 			if(!allNames.contains(this.m.get(i).getStationName())) {
+ 				allNames.add(this.m.get(i).getStationName());
+ 			}
+		}
+		return Collections.unmodifiableList(allNames);
+	}
+
+	//For testing purposes
+	public static void main(String[] args) {
+		Queries aQ = new Queries();
+		DailyReport aRep = aQ.getSpecificDailyReport("LAWRENCE MUNICIPAL AIRPORT", "4/2/2022");
+		System.out.println(aRep.toString());
+	}
 }
